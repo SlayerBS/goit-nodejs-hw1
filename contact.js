359e9d129098cfed882eca882d600ac5cf408f50
Sergii Bogush <sergbogush@gmail.com>
2021-09-22 14:08:21 +0300

    Update contact.js

--- contact.js
+++ contact.js
@@ -2,10 +2,6 @@ const fs = require("fs/promises");
 const path = require("path");
 const crypto = require("crypto");
 
-/*
- * Раскомментируй и запиши значение
- * const contactsPath = ;
- */
 const readContacts = async () => {
   const result = await fs.readFile(
     path.join(__dirname, "contacts.json"),
@@ -14,7 +10,6 @@ const readContacts = async () => {
   const contacts = JSON.parse(result);
   return contacts;
 };
-// TODO: задокументировать каждую функцию
 function listContacts() {
   return readContacts();
 }
@@ -28,9 +23,10 @@ async function getContactById(contactId) {
 async function removeContact(contactId) {
   const contacts = await readContacts();
   let index = -1;
-  contacts.forEach((contact, idx) => { console.log('idx', idx, 'contact.id', contact.id); if (contact.id === contactId)
-  index = idx;})
-  if (index>-1){
+  contacts.forEach((contact, idx) => {
+    if (contact.id === contactId) index = idx;
+  });
+  if (index > -1) {
     contacts.splice(index, 1);
   }
   await fs.writeFile(
@@ -38,13 +34,11 @@ async function removeContact(contactId) {
     JSON.stringify(contacts, null, 2)
   );
 
-return index;
- 
+  return index;
 }
 
 async function addContact(name, email, phone) {
   const contacts = await readContacts();
-  console.log(contacts);
   const newContact = { id: crypto.randomUUID(), name, email, phone };
   contacts.push(newContact);
   await fs.writeFile(
